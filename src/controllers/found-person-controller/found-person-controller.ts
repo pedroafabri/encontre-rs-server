02df7b5d2e50c0528121d75678e415667a2559ad
pedroafabri <pedroafabri@gmail.com>
2024-05-08 19:52:55 -0300

    Multer base ready

--- src/controllers/found-person-controller/found-person-controller.ts
+++ src/controllers/found-person-controller/found-person-controller.ts
@@ -1,15 +1,30 @@
 import {AUTH, POST} from "../../decorators";
 import {Request, Response, NextFunction} from "express";
+import multer from "multer";
+import { promisify } from "util";
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 2 * 1024 * 1024, // limit file size to 2MB
+    },
+});
 
 export class FoundPersonController {
 
     @POST('/found-person')
     @AUTH()
-    async createFoundPerson(req: Request, res: Response, next: NextFunction) {
-        try {
-            //TODO: Call service
-        } catch(e) {
-            next(e);
+    async createFoundPerson(request: Request, response: Response, next: NextFunction) {
+        const multerHandler = promisify(upload.single("image"));
+
+        await multerHandler(request, response);
+        const fileDetails = request.file;
+        if (fileDetails) {
+            console.log('Arquivo carregado:', fileDetails.originalname);
+        } else {
+            next(new Error('Nenhum arquivo foi carregado'));
         }
+
+        response.status(200).send('Upload bem-sucedido');
     }
 }
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,7 @@
 import {UserController} from "./user-controller";
+import {FoundPersonController} from "./found-person-controller";
 
 export const CONTROLLERS = [
     UserController,
+    FoundPersonController
 ]
